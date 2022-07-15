6c7e36b99b6b3d4184fefb99c7576e967f27b45b
naman jain <naman.nishi@gmail.com>
2022-07-15 11:45:16 +0530

    First Mvc
    Added the main files and some edits may be eft and it contains a login page where you can easily signup and all view all the employees and connection with the help of dapers.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using static DataLibrarary.Logic.EmpProcess;
+using DataLibrarary.Model;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employee List";
+            var data = Load<Emp>();
+            List<Emp> employees = new List<Emp>();
+            foreach(var row in data)
+            {
+                employees.Add(new Emp
+                {
+                    EmployeId = row.EmployeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                }) ;
+            }
+            return View(employees);
+        }
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Employee SignUp";
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(Emp model)
+        {
+            //Validation
+            if(ModelState.IsValid)
+            {
+                int recordCreated=CreateEmp(model.EmployeId,
+                    model.FirstName,
+                    model.LastName,
+                    model.EmailAddress);
+
+                return RedirectToAction("Index"); 
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Emp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Emp
+    {
+        [Range(100,9999,ErrorMessage ="Enter a Valid Id")]
+        [Display(Name ="Your Id")]
+        public int EmployeId { get; set; }
+        
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string EmailAddress { get; set; }
+        
+        [DataType(DataType.Password)]//To have non viewable
+        [StringLength(100,MinimumLength =8,ErrorMessage ="Enter A valid password of max 100 and min 8 characters")]
+        [Required]
+        public string password { get; set; }
+        
+        [Required]
+        [Compare("password",ErrorMessage="Enter Same as original password")]//We used to compare
+        [DataType(DataType.Password)]//To have non viewable
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Enter A valid password of max 100 and min 8 characters")]
+        public string confirmpassword { get; set; }
+
+    }
+}
